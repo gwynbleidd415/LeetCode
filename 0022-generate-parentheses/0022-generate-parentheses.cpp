75088dfcff49334ac5460ba15a207053595c08cb
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-26 08:45:58 +0530

    Time: 0 ms (100%), Space: 8.4 MB (95.33%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,17 +1,36 @@
 class Solution {
-public:
-    vector<string> generateParenthesis(int n) {
-        if(n == 0) return {""};
+    vector<string> generateParenthesisHelper(int n, vector<vector<string>> &dp, vector<bool> &visited) {
+        if(visited[n]) return dp[n];
         vector<string> inner, outer, ans;
         for(int i{0};i<n;++i){
-            inner = generateParenthesis(i);
-            outer = generateParenthesis(n-i-1);
+            inner = generateParenthesisHelper(i, dp, visited);
+            outer = generateParenthesisHelper(n-i-1, dp, visited);
             for(string &str: inner){
                 for(string &str1: outer){
                     ans.push_back("(" + str + ")" + str1);
                 }
             }
         }
-        return ans;
+        visited[n] = true;
+        return dp[n] = ans;
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        // if(n == 0) return {""};
+        // vector<string> inner, outer, ans;
+        // for(int i{0};i<n;++i){
+        //     inner = generateParenthesis(i);
+        //     outer = generateParenthesis(n-i-1);
+        //     for(string &str: inner){
+        //         for(string &str1: outer){
+        //             ans.push_back("(" + str + ")" + str1);
+        //         }
+        //     }
+        // }
+        // return ans;
+        vector<vector<string>> dp(n+1);
+        vector<bool> visited(n+1, false);
+        dp[0] = {""}, visited[0] = true;
+        return generateParenthesisHelper(n, dp, visited);
     }
 };
