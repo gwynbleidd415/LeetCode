18046c84873f8cab847380eb7976b80b8a5ad201
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-26 12:40:56 +0530

    Time: 0 ms (100%), Space: 6.8 MB (13.69%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,5 @@
 class Solution {
-public:
-    int strStr(string haystack, string needle) {
+    int bruteForce(string &haystack, string &needle){
         int n = haystack.size(), m = needle.size(), j, k;
         for(int i{0}; i <= n-m;++i){
             j = 0, k = i;
@@ -10,5 +9,33 @@ public:
             if(j == m) return i;
         }
         return -1;
+    }
+    int zAlgorithm(string &haystack, string &needle){
+        string str = needle + "#" + haystack;
+        int n = str.size(), m = needle.size();
+        vector<int> zarr(n, 0);
+        int i{1}, j{0}, k{1};
+        while(i<n){
+            while(k<n && str[k] == str[j]){
+                ++j, ++k;
+            }
+            if(j == m){
+                return i - (m+1);
+            }
+            zarr[i] = j;
+            j = 1; ++i;
+            while(zarr[j] + i < k){
+                zarr[i] = zarr[j];
+                ++i, ++j;
+            }
+            k = max(k, i);
+            j = k-i;
+        }
+        return -1;
+    }
+public:
+    int strStr(string haystack, string needle) {
+        // return bruteForce(haystack, needle);
+        return zAlgorithm(haystack, needle);
     }
 };
