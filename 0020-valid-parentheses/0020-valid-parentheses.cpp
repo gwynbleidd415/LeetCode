3c791de73f219bb71c78733510b628ffb0a414cc
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-26 07:34:11 +0530

    Time: 0 ms (100%), Space: 6.9 MB (14.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -5,11 +5,11 @@ public:
         for(char ch: s){
             if(ch == '(' || ch == '{' || ch == '[') st.emplace(ch);
             else{
-                if(st.size() == (size_t)0 || ch == ')' && st.top() != '(' || ch == '}' && st.top() != '{' || ch == ']' && st.top() != '[') return false;
+                if(st.empty()|| ch == ')' && st.top() != '(' || ch == '}' && st.top() != '{' || ch == ']' && st.top() != '[') return false;
                 else st.pop();
             }
         }
-        if(st.size() != (size_t)0) return false;
+        if(!st.empty()) return false;
         return true;
     }
 };
