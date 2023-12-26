095e0afb425a728964aab4d21c16ab678a0f2f7b
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-26 15:11:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:
-    int divide(int dividend, int divisor) {
+private:
+    int divideHelperPositive(int dividend, int divisor) {
         if(dividend == INT32_MIN && divisor == -1) return INT32_MAX;
         if(divisor == INT32_MIN){
             if(dividend == INT32_MIN) return 1;
@@ -40,5 +40,36 @@ public:
             ++ans;
         }
         return isNeg?-ans:ans;
+    }
+    int divideHelperNegative(int dividend, int divisor) {
+        if(dividend == 0) return 0;
+        if(dividend == INT32_MIN && divisor == -1) return INT32_MAX;
+
+        bool isNeg = (dividend ^ divisor) < 0;
+        if(dividend > 0) dividend = -dividend;
+        if(divisor > 0) divisor = -divisor;
+        int temp = divisor, itr{1}, ans{0}, temp1;
+        while(dividend <= temp){
+            dividend -= temp;
+            ans -= itr;
+            temp1 = temp << 1;
+            if(temp1 == INT_MIN) break;
+            temp = temp1;
+            itr <<= 1;
+        }
+        while(dividend < 0){
+            while(temp < dividend){
+                temp >>= 1;
+                itr >>= 1;
+            }
+            if(temp > divisor) break;
+            ans -= itr;
+            dividend -= temp;
+        }
+        return isNeg?ans:-ans;
+    }
+public:
+    int divide(int dividend, int divisor) {
+        return divideHelperNegative(dividend, divisor);
     }
 };
