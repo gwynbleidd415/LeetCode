19da57d43eeff4561f52ef78c751efde023722d0
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-26 09:55:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,26 +11,45 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode *mergedList = new ListNode();
-        ListNode *tail = mergedList;
-        int mi, n = lists.size();
-        while(true){
-            mi = -1;
-            for(int i = 0; i < n; ++i){
-                if(!lists[i]) continue;
-                if(mi == -1){
-                    mi = i;
-                    continue;
-                }
-                if(lists[i]->val < lists[mi]->val){
-                    mi = i;
-                }
-            }
-            if(mi == -1) break;
-            tail->next = lists[mi];
-            tail = tail->next;
-            lists[mi] = lists[mi]->next;
+        // ListNode *mergedList = new ListNode();
+        // ListNode *tail = mergedList;
+        // int mi, n = lists.size();
+        // while(true){
+        //     mi = -1;
+        //     for(int i = 0; i < n; ++i){
+        //         if(!lists[i]) continue;
+        //         if(mi == -1){
+        //             mi = i;
+        //             continue;
+        //         }
+        //         if(lists[i]->val < lists[mi]->val){
+        //             mi = i;
+        //         }
+        //     }
+        //     if(mi == -1) break;
+        //     tail->next = lists[mi];
+        //     tail = tail->next;
+        //     lists[mi] = lists[mi]->next;
+        // }
+        // return mergedList->next;
+
+        auto pqcomp = [](ListNode *a, ListNode *b){
+            return a->val > b->val;
+        };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(pqcomp)> pq(pqcomp);
+        for(ListNode* &list: lists){
+            while(list){
+                pq.push(list);
+                list = list->next;
+            } 
         }
-        return mergedList->next;
+        ListNode mergedList(0);
+        ListNode *curr = &mergedList;
+        while(!pq.empty()){
+            curr->next = pq.top();
+            curr = curr->next;
+            pq.pop();
+        }
+        return mergedList.next;
     }
 };
