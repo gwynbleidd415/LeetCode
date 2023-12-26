8a8066830c9b9914f7340c9a64fcdc6a6a68849c
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-26 07:49:03 +0530

    Time: 3 ms (85.35%), Space: 15.3 MB (24.7%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -17,21 +17,32 @@ private:
     }
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* list3 = new ListNode();
-        ListNode* list3Head = list3;
-        while(list1 && list2){
-            if(list1->val < list2->val){
-                list3Next(&list3, &list1);
-            } else {
-                list3Next(&list3, &list2);
-            }
+        // ListNode* list3 = new ListNode();
+        // ListNode* list3Head = list3;
+        // while(list1 && list2){
+        //     if(list1->val < list2->val){
+        //         list3Next(&list3, &list1);
+        //     } else {
+        //         list3Next(&list3, &list2);
+        //     }
+        // }
+        // while(list1){
+        //     list3Next(&list3, &list1);
+        // }
+        // while(list2){
+        //     list3Next(&list3, &list2);
+        // }
+        // return list3Head->next;
+
+        if(!list1) return list2;
+        if(!list2) return list1;
+
+        if(list1->val < list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
         }
-        while(list1){
-            list3Next(&list3, &list1);
-        }
-        while(list2){
-            list3Next(&list3, &list2);
-        }
-        return list3Head->next;
     }
 };
