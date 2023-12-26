edcaefd58ef7bb74bc082b8e8f13f8d5f9165c33
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-26 06:35:04 +0530

    Time: 0 ms (100%), Space: 6.9 MB (43.95%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,40 @@
 class Solution {
 private:
+    // static const vector<string> map;
     inline static const vector<string> map{"", "", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    void letterCombinationsHelper(string &digits, string &currStr, vector<string> &ans, int pos){
-        if(pos == digits.size()) {
-            ans.push_back(currStr);
-            return;
-        };
+    // void letterCombinationsHelper(string &digits, string &currStr, vector<string> &ans, int pos){
+    //     if(pos == digits.size()) {
+    //         ans.push_back(currStr);
+    //         return;
+    //     };
         
-        for(char ch: map[digits[pos] - '0']){
-            currStr.push_back(ch);
-            letterCombinationsHelper(digits, currStr, ans, pos+1);
-            currStr.pop_back();
-        }
-    }
+    //     for(char ch: map[digits[pos] - '0']){
+    //         currStr.push_back(ch);
+    //         letterCombinationsHelper(digits, currStr, ans, pos+1);
+    //         currStr.pop_back();
+    //     }
+    // }
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        if(!digits.size()) return ans;
-        string currStr = "";
-        letterCombinationsHelper(digits, currStr, ans, 0);
+        // vector<string> ans;
+        // if(!digits.size()) return ans;
+        // string currStr = "";
+        // letterCombinationsHelper(digits, currStr, ans, 0);
+        // return ans;
+
+        if(!digits.size()) return {};
+        
+        vector<string> ans{""};
+        for(char ch: digits){
+            vector<string> curr;
+            for(char ch1: map[ch-'0']){
+                for(string &str: ans){
+                    curr.push_back(str + string(1, ch1));
+                }
+            }
+            ans = curr;
+        }
         return ans;
     }
 };
+// const vector<string> Solution::map{"", "", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
