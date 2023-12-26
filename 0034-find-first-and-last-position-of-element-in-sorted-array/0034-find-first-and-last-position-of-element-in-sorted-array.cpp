3d325d7e2d524377e721cca6a694e3006f545917
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-26 17:42:59 +0530

    Time: 3 ms (92.85%), Space: 13.9 MB (96.42%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,18 +3,18 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         if(nums.empty()) return {-1, -1};
         int startPos, endPos;
-        int l{0}, r{(int)nums.size()-1}, mid, pa;
+        int l{0}, r{(int)nums.size()-1}, mid, pa{0};
         while(l<=r){
             mid = (l+r)>>1;
             if(nums[mid] >= target) r = mid-1, pa=mid;
             else l = mid+1;
         }
-        // cout << l << ' ' << r << ' ' << mid << '\n';
+        // cout << l << ' ' << r << ' ' << mid << ' ' << pa << '\n';
         if(nums[pa]!=target) return {-1,-1};
         else startPos = l;
         l = 0, r = nums.size()-1;
         while(l<=r){
-            mid = (l+r+1)>>1;
+            mid = (l+r)>>1;
             if(nums[mid]<=target) l = mid+1, pa = mid;
             else r = mid-1;
         }
