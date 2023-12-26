338bd5b58141cee6f146e12624b3388cc4ce06d1
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-26 21:26:19 +0530

    Time: 35 ms (98.48%), Space: 41.5 MB (82%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,12 +3,8 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size(), i{0};
         while(i<n){
-            if(nums[i] < 1 || nums[i] >= n) {
-                ++i;
-                continue;
-            }
-            if(nums[i] != i && nums[nums[i]] != nums[i]) swap(nums[i], nums[nums[i]]);
-            else ++i;
+            if(nums[i] < 1 || nums[i] >= n || nums[i] == i || nums[nums[i]] == nums[i]) ++i;
+            else swap(nums[i], nums[nums[i]]);
         }
         for(i=1;i<n;++i){
             if(nums[i] != i) return i;
