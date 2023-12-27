d59184a41045761be55822424fa139b61ad46b9d
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 01:18:14 +0530

    Time: 20 ms (96.11%), Space: 20.2 MB (71.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -6,15 +6,10 @@ public:
         for(string &str: strs){
             str1 = str;
             sort(str1.begin(), str1.end());
-            auto uitr = umap.find(str1);
-            if(uitr != umap.end()){
-                (uitr->second).push_back(str);
-            } else {
-                umap.insert({str1, {str}});
-            }
+            umap[str1].push_back(str);
         }
         vector<vector<string>> ans;
-        for(auto &umapel: umap){
+        for(pair<const string, vector<string>> &umapel: umap){
             ans.push_back(umapel.second);
         }
         return ans;
