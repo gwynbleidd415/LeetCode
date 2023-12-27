03b9d71dc7f5002690126a22820308e355d010f2
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 01:32:34 +0530

    Time: 3 ms (42.13%), Space: 6.3 MB (60.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        // double res{1};
+        // if(n<0) {
+        //     x = 1/x;
+        //     if(n == INT_MIN){
+        //         n = INT_MAX;
+        //         res = x;
+        //     } else {
+        //         n = -n;
+        //     }
+        // }
+        // while(n){
+        //     if(n&1) res *= x;
+        //     x *= x;
+        //     n >>= 1;
+        // }
+        // return res;
+
         double res{1};
+        unsigned exp = n;
         if(n<0) {
-            x = 1/x;
-            if(n == INT_MIN){
-                n = INT_MAX;
-                res = x;
-            } else {
-                n = -n;
-            }
+            exp = -exp;
         }
-        while(n){
-            if(n&1) res *= x;
+        while(exp){
+            if(exp & 1) res *= x;
             x *= x;
-            n >>= 1;
+            exp >>= 1;
         }
+        if(n<0) res = 1/res;
         return res;
     }
 };
