114237d383affb561e8a233128b4b4dd1e8da63d
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-27 22:28:25 +0530

    Time: 27 ms (56.22%), Space: 89.6 MB (69.93%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size(), m = nums2.size();
         int ap = 0, bp = 0;
-        int length = nums1.size() + nums2.size();
+        int length = n + m;
         int mid = (length - 1) / 2;
 
-        while(ap+bp < mid && ap < nums1.size() && bp < nums2.size()) {
+        while(ap+bp < mid && ap < n && bp < m) {
             if(nums1[ap] < nums2[bp]){
                 ++ap;
             } else {
                 ++bp;
             }
         }
-        if(ap == nums1.size()){
+
+        if(ap == n){
             if(length & 1){
                 return nums2[mid-ap];
             }
             return (double)(nums2[mid-ap] + nums2[mid-ap+1])/2;
         }
-        if(bp == nums2.size()){
+        if(bp == m){
             if(length & 1){
                 return nums1[mid-bp];
             }
@@ -28,12 +30,12 @@ public:
             return min(nums1[ap], nums2[bp]);
         }
         if(nums1[ap] < nums2[bp]){
-            if(ap != nums1.size() - 1){
+            if(ap != n - 1){
                 return (double)(nums1[ap] + min(nums1[ap+1], nums2[bp]))/2;
             }
             return (double)(nums1[ap] + nums2[bp]) / 2;
         }
-        if(bp != nums2.size() - 1) {
+        if(bp != m - 1) {
             return (double)(nums2[bp] + min(nums2[bp+1], nums1[ap]))/2;
         }
         return (double)(nums1[ap] + nums2[bp]) / 2;
