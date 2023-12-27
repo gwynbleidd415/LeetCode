08ba2aa97271d758707737b074e0cfd873d67db6
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 01:01:11 +0530

    Time: 2 ms (45.5%), Space: 7.7 MB (5.7%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -13,15 +13,14 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size(), m = (n >> 1), n1=n-1;
-        int x, y;
+        int x, y, x1, y1;
         for(int i{0};i<m;++i){
             for(int j{i};j<n1-i;++j){
                 x = i, y = j;
                 for(int k{0};k<3;++k){
-                    swap(matrix[x][y], matrix[n1-y][x]);
-                    x = x^y;
-                    y = x^y;
-                    x = n1 - (x^y);
+                    x1 = n1-y, y1 = x;
+                    swap(matrix[x][y], matrix[x1][y1]);
+                    x = x1, y = y1;
                 }
                 // printMatrix(matrix);
             }
