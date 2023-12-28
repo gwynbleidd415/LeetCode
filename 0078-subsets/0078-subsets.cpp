9f51897026824d31ac6a94adfe052d4b46f9695e
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 20:50:50 +0530

    Time: 0 ms (100%), Space: 7.2 MB (99.05%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,37 @@
 class Solution {
 private:
-    vector<vector<int>> subsetsHelper(vector<int>& nums, int n){
-        if(n == -1) return {{}};
+    // vector<vector<int>> subsetsHelper(vector<int>& nums, int n){
+    //     if(n == -1) return {{}};
 
-        vector<vector<int>> subAns = subsetsHelper(nums, n-1);
-        vector<int> arr;
-        int m = subAns.size();
-        for(int i{0}; i< m;++i){
-            arr = subAns[i];
-            arr.push_back(nums[n]);
-            subAns.push_back(arr);
-        }
-        return subAns;
-    }
+    //     vector<vector<int>> subAns = subsetsHelper(nums, n-1);
+    //     vector<int> arr;
+    //     int m = subAns.size();
+    //     for(int i{0}; i< m;++i){
+    //         arr = subAns[i];
+    //         arr.push_back(nums[n]);
+    //         subAns.push_back(arr);
+    //     }
+    //     return subAns;
+    // }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        if(nums.size() == 0) return {{}};
+        // if(nums.size() == 0) return {{}};
 
-        return subsetsHelper(nums, nums.size() - 1);
+        // return subsetsHelper(nums, nums.size() - 1);
+
+        vector<vector<int>> ans{{}};
+        vector<int> arr;
+
+        int n{(int)nums.size()}, m;
+        for(int i{0};i<n;++i){
+            m = ans.size();
+            for(int j{0};j<m;++j){
+                arr = ans[j];
+                arr.push_back(nums[i]);
+                ans.push_back(arr);
+            }
+        }
+        return ans;
+
     }
 };
