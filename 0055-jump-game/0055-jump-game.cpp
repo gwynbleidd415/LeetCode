3d72ff53470924bfa0328c3b14b46aad1a755d64
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 13:47:17 +0530

    Time: 135 ms (20.72%), Space: 50.9 MB (13.02%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -8,23 +8,33 @@ private:
     // }
 public:
     bool canJump(vector<int>& nums) {
+        // int n{(int)nums.size()};
+        // vector<bool> canJump(n);
+        // canJump[n-1] = true;
+        // int temp{};
+        // for(int i{n-2};i>=0;--i){
+        //     temp = nums[i] + i + 1;
+        //     if(temp >= n) {
+        //         canJump[i] = true;
+        //         continue;
+        //     }
+        //     for(int j{i+1};j<temp;++j){
+        //         if(canJump[j]) {
+        //             canJump[i] = true;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return canJump[0];
+
         int n{(int)nums.size()};
-        vector<bool> canJump(n);
-        canJump[n-1] = true;
+        vector<int> lji(n);
+        lji.back() = n-1;
         int temp{};
-        for(int i{n-2};i>=0;--i){
-            temp = nums[i] + i + 1;
-            if(temp >= n) {
-                canJump[i] = true;
-                continue;
-            }
-            for(int j{i+1};j<temp;++j){
-                if(canJump[j]) {
-                    canJump[i] = true;
-                    break;
-                }
-            }
+        for(int i{n-2}; i>=0 ; --i){
+            if(nums[i]+i >= lji[i+1]) lji[i] = i;
+            else lji[i] = lji[i+1];
         }
-        return canJump[0];
+        return lji.front() == 0;
     }
 };
