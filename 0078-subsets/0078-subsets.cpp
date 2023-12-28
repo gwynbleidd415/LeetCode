606401495a3ce094ee13d2908306f4f11bac2a73
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 20:47:21 +0530

    Time: 0 ms (100%), Space: 7.6 MB (60.03%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    vector<vector<int>> subsetsHelper(vector<int>& nums, int n){
+        if(n == -1) return {{}};
+
+        vector<vector<int>> subAns = subsetsHelper(nums, n-1);
+        vector<int> arr;
+        int m = subAns.size();
+        for(int i{0}; i< m;++i){
+            arr = subAns[i];
+            arr.push_back(nums[n]);
+            subAns.push_back(arr);
+        }
+        return subAns;
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        if(nums.size() == 0) return {{}};
+
+        return subsetsHelper(nums, nums.size() - 1);
+    }
+};
