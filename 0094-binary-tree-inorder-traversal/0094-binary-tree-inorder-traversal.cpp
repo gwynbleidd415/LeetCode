c52a096f83d13ca17803e8d1ff53048ad38afc8c
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-29 02:07:26 +0530

    Time: 5 ms (33.13%), Space: 8.7 MB (78.58%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,16 +10,32 @@
  * };
  */
 class Solution {
-    void inorderTraversalHelper(TreeNode *root, vector<int> &ans){
+    // void inorderTraversalHelper(TreeNode *root, vector<int> &ans){
+    //     if(root == nullptr) return;
+    //     inorderTraversalHelper(root->left, ans);
+    //     ans.push_back(root->val);
+    //     inorderTraversalHelper(root->right, ans);
+    // }
+    void inorderTraversalHelperStack(TreeNode *root, vector<int> &ans){
         if(root == nullptr) return;
-        inorderTraversalHelper(root->left, ans);
-        ans.push_back(root->val);
-        inorderTraversalHelper(root->right, ans);
+        stack<TreeNode *> st;
+        TreeNode *temp, *curr{root};
+        while(curr || !st.empty()){
+            while(curr){
+                st.push(curr);
+                curr = curr->left;
+            }
+            temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            curr = temp->right;
+        }
     }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inorderTraversalHelper(root, ans);
+        // inorderTraversalHelper(root, ans);
+        inorderTraversalHelperStack(root, ans);
         return ans;
     }
 };
