20d41a7204e5cea5591486336f45ff2e5c8344b5
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 13:25:29 +0530

    Time: 0 ms (100%), Space: 7.3 MB (60.73%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,22 +2,51 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int w{0}, x{(int)matrix.size()-1}, z{(int)matrix.front().size()-1};
-        vector<int> ans;
-        ans.reserve((x+1)*(z+1));
-        int i{-1}, j{-1};
-        while(w<x && w<z){
-            // cout<<w<<' '<<x<<' '<<z <<' '<<i+1<<' '<<j+1<<'\n';
-            for(++i, ++j;j<=z;++j) ans.push_back(matrix[i][j]);
-            for(++i, --j;i<=x;++i) ans.push_back(matrix[i][j]);
-            for(--i, --j;j>=w;--j) ans.push_back(matrix[i][j]);
-            for(--i, ++j;i>w;--i) ans.push_back(matrix[i][j]);
-            ++w, --x, --z;
+        int an{(x+1)*(z+1)};
+        vector<int> ans(an);
+        ans[0] = matrix.front().front();
+        int i{0}, j{1};
+        if(matrix.front().size() == 1){
+            j = 0, i = 1;
         }
-        if(w == x){
-            for(++i, ++j;j<=z;++j) ans.push_back(matrix[i][j]);
-        } else if(w == z) {
-            for(++i, ++j;i<=x;++i) ans.push_back(matrix[i][j]);
+        for(int k{1};k<an;++k){
+            if(i == w && j!=w) {
+                if(j<z) ans[k] = matrix[i][j++];
+                else ans[k] = matrix[i++][j];
+            } else if(j == z) {
+                if(i<x) ans[k] = matrix[i++][j];
+                else ans[k] = matrix[i][j--];
+            } else if(i == x) {
+                if(j>w) ans[k] = matrix[i][j--];
+                else ans[k] = matrix[i--][j];
+            } else {
+                if(i>w) ans[k] = matrix[i--][j];
+                else {
+                    ans[k] = matrix[++i][++j];
+                    ++w, --x, --z;
+                    if(j == z) ++i;
+                    else ++j;
+                }
+            }
         }
         return ans;
+        // int w{0}, x{(int)matrix.size()-1}, z{(int)matrix.front().size()-1};
+        // vector<int> ans;
+        // ans.reserve((x+1)*(z+1));
+        // int i{-1}, j{-1};
+        // while(w<x && w<z){
+        //     // cout<<w<<' '<<x<<' '<<z <<' '<<i+1<<' '<<j+1<<'\n';
+        //     for(++i, ++j;j<=z;++j) ans.push_back(matrix[i][j]);
+        //     for(++i, --j;i<=x;++i) ans.push_back(matrix[i][j]);
+        //     for(--i, --j;j>=w;--j) ans.push_back(matrix[i][j]);
+        //     for(--i, ++j;i>w;--i) ans.push_back(matrix[i][j]);
+        //     ++w, --x, --z;
+        // }
+        // if(w == x){
+        //     for(++i, ++j;j<=z;++j) ans.push_back(matrix[i][j]);
+        // } else if(w == z) {
+        //     for(++i, ++j;i<=x;++i) ans.push_back(matrix[i][j]);
+        // }
+        // return ans;
     }
 };
