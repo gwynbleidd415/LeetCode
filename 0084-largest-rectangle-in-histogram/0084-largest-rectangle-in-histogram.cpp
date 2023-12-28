2a96de1e79558766550d2430ea66d4cdddbb4784
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-29 00:21:06 +0530

    Time: 147 ms (48.45%), Space: 75.7 MB (92.39%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -8,21 +8,9 @@ const static int initialize = [] {
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int> st;
-        st.push(-1);
-        heights.push_back(0);
-        int ans{0}, n{(int)heights.size()}, tempTop;
-        for(int i{0};i<n;++i){
-            while(st.size() > 1 && heights[st.top()] >= heights[i]){
-                tempTop = st.top();
-                st.pop();
-                ans = max(ans, (i-st.top()-1)*heights[tempTop]);
-            }
-            st.push(i);
-        }
-        return ans;
         // stack<int> st;
         // st.push(-1);
+        // heights.push_back(0);
         // int ans{0}, n{(int)heights.size()}, tempTop;
         // for(int i{0};i<n;++i){
         //     while(st.size() > 1 && heights[st.top()] >= heights[i]){
@@ -32,11 +20,23 @@ public:
         //     }
         //     st.push(i);
         // }
-        // while(st.size()>1){
-        //     tempTop = st.top();
-        //     st.pop();
-        //     ans = max(ans, (n-st.top()-1)*heights[tempTop]);
-        // }
         // return ans;
+        stack<int> st;
+        st.push(-1);
+        int ans{0}, n{(int)heights.size()}, tempTop;
+        for(int i{0};i<n;++i){
+            while(st.size() > 1 && heights[st.top()] >= heights[i]){
+                tempTop = st.top();
+                st.pop();
+                ans = max(ans, (i-st.top()-1)*heights[tempTop]);
+            }
+            st.push(i);
+        }
+        while(st.size()>1){
+            tempTop = st.top();
+            st.pop();
+            ans = max(ans, (n-st.top()-1)*heights[tempTop]);
+        }
+        return ans;
     }
 };
