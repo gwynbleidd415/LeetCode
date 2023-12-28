702ab6852bc0597205c1e94660959b474b01ff2a
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-29 01:03:22 +0530

    Time: 0 ms (100%), Space: 6.5 MB (86.69%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -9,7 +9,7 @@ public:
                 n2 = n1;
                 n1 = 0;
             } else {
-                if(s.substr(i, 2) <= "26"){
+                if(s[i] == '1' || (s[i] == '2' && s[i+1] < '7')){
                     n1 = n1+n2;
                     n2 = n1-n2;
                 } else {
@@ -18,5 +18,22 @@ public:
             }
         }
         return n1;
+        // int n{(int)s.size()};
+        // int n1{1}, n2{1};
+        // if(s.back() == '0') n1 = 0;
+        // for(int i{n-2};i>=0;--i){
+        //     if(s[i] == '0') {
+        //         n2 = n1;
+        //         n1 = 0;
+        //     } else {
+        //         if(s.substr(i, 2) <= "26"){
+        //             n1 = n1+n2;
+        //             n2 = n1-n2;
+        //         } else {
+        //             n2 = n1;
+        //         }
+        //     }
+        // }
+        // return n1;
     }
 };
