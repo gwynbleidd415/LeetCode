a74fd571a00baa96998c4662b7a20a626e056e66
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 20:29:40 +0530

    Time: 10 ms (69%), Space: 8.2 MB (69.38%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,29 +3,15 @@ public:
     string minWindow(string s, string t) {
         int m{(int)s.size()}, n{(int)t.size()};
         vector<int> charr(128, 0);
-        vector<bool> ccharr(128, 0);
         int toPos{0};
-        for(char ch:t){
-            ++charr[ch];
-        }
-        for(int i{0};i<128;++i){
-            if(charr[i]) ccharr[i] = true, ++toPos;
-        }
+        for(char ch:t) if(++charr[ch] == 1) ++toPos;
         int l{0}, r{0};
         int al{0}, len{INT_MAX};
         while(r<m){
-            if(ccharr[s[r]]){
-                if(--charr[s[r]] == 0) --toPos;
-            }
-            ++r;
-            if(toPos) {
-                continue;
-            }
+            if(--charr[s[r++]] == 0) --toPos;
+            if(toPos) continue;
             while(toPos == 0){
-                if(ccharr[s[l]]){
-                    if(++charr[s[l]] == 1) ++toPos;
-                }
-                ++l;
+                if(++charr[s[l++]] == 1) ++toPos;
             }
             if(r-l+1 < len) {
                 al = l-1;
@@ -34,5 +20,39 @@ public:
         }
         if(len == INT_MAX) return "";
         return s.substr(al, len);
+
+        // int m{(int)s.size()}, n{(int)t.size()};
+        // vector<int> charr(128, 0);
+        // vector<bool> ccharr(128, 0);
+        // int toPos{0};
+        // for(char ch:t){
+        //     ++charr[ch];
+        // }
+        // for(int i{0};i<128;++i){
+        //     if(charr[i]) ccharr[i] = true, ++toPos;
+        // }
+        // int l{0}, r{0};
+        // int al{0}, len{INT_MAX};
+        // while(r<m){
+        //     if(ccharr[s[r]]){
+        //         if(--charr[s[r]] == 0) --toPos;
+        //     }
+        //     ++r;
+        //     if(toPos) {
+        //         continue;
+        //     }
+        //     while(toPos == 0){
+        //         if(ccharr[s[l]]){
+        //             if(++charr[s[l]] == 1) ++toPos;
+        //         }
+        //         ++l;
+        //     }
+        //     if(r-l+1 < len) {
+        //         al = l-1;
+        //         len = r-l+1;
+        //     }
+        // }
+        // if(len == INT_MAX) return "";
+        // return s.substr(al, len);
     }
 };
