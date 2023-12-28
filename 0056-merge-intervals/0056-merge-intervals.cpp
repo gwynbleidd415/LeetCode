553558892cebe2aef6da80028964980ab090d720
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 13:58:04 +0530

    Time: 52 ms (9.68%), Space: 19.4 MB (80.21%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        int n{(int)intervals.size()};
+        int si{intervals[0][0]}, ei{intervals[0][1]};
+        for(int i{1};i<n;++i){
+            if(intervals[i][0] > ei) {
+                ans.push_back({si, ei});
+                si = intervals[i][0], ei = intervals[i][1];
+            } else {
+                ei = max(ei, intervals[i][1]);
+            }
+        }
+        ans.push_back({si, ei});
+        return ans;
+    }
+};
