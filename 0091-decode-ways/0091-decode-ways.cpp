fe421d8fb0206f60386849466870181cdea96492
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-29 01:01:39 +0530

    Time: 6 ms (7.47%), Space: 6.3 MB (97.95%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n{(int)s.size()};
+        int n1{1}, n2{1};
+        if(s.back() == '0') n1 = 0;
+        for(int i{n-2};i>=0;--i){
+            if(s[i] == '0') {
+                n2 = n1;
+                n1 = 0;
+            } else {
+                if(s.substr(i, 2) <= "26"){
+                    n1 = n1+n2;
+                    n2 = n1-n2;
+                } else {
+                    n2 = n1;
+                }
+            }
+        }
+        return n1;
+    }
+};
