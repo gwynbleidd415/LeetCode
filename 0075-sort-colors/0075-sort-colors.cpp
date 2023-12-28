9d8e54896193fc07be280e06d1a01357d778dc48
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 18:03:36 +0530

    Time: 0 ms (100%), Space: 8.5 MB (80.28%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -10,17 +10,28 @@ public:
         //     }
         // }
 
-        int itr0{0}, itr1{0}, itr2{(int)nums.size()-1};
-        while(itr1 <= itr2){
-            if(nums[itr1] == 0){
-                swap(nums[itr0], nums[itr1]);
-                ++itr0, ++itr1;
-            } else if(nums[itr1] == 1){
-                ++itr1;
-            } else {
-                swap(nums[itr1],nums[itr2]);
-                --itr2;
-            }
+        // int itr0{0}, itr1{0}, itr2{(int)nums.size()-1};
+        // while(itr1 <= itr2){
+        //     if(nums[itr1] == 0){
+        //         swap(nums[itr0], nums[itr1]);
+        //         ++itr0, ++itr1;
+        //     } else if(nums[itr1] == 1){
+        //         ++itr1;
+        //     } else {
+        //         swap(nums[itr1],nums[itr2]);
+        //         --itr2;
+        //     }
+        // }
+
+        vector<int> hm(3, 0);
+        for(int num:nums) ++hm[num];
+        hm[1] += hm[0];
+        hm[2] += hm[1];
+        int n{(int)nums.size()};
+        for(int i{0};i<n;++i){
+            if(i < hm[0]) nums[i] = 0;
+            else if(i < hm[1]) nums[i] = 1;
+            else nums[i] = 2;
         }
     }
 };
