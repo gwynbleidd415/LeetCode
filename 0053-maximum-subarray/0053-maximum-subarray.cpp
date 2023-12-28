39115510d834ebd9524f688e61a9a5f6abcf35ab
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-28 12:07:47 +0530

    Time: 128 ms (5.2%), Space: 68.1 MB (27.55%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans{0}, maxx{INT_MIN}, n{(int)nums.size()};
-        int l{0}, r{0} ,tSum{0};
+        // int ans{0}, maxx{INT_MIN}, n{(int)nums.size()};
+        // int l{0}, r{0} ,tSum{0};
+        // while(r<n){
+        //     maxx = max(maxx, nums[r]);
+        //     tSum += nums[r++];
+        //     while(l<r && tSum < 0) tSum -= nums[l++];
+        //     ans = max(tSum, ans);
+        // }
+        // if(maxx <= 0) return maxx;
+        // return ans;
+        int ans{INT32_MIN}, n{(int)nums.size()};
+        int r{0} ,tSum{0};
         while(r<n){
-            maxx = max(maxx, nums[r]);
-            tSum += nums[r++];
-            while(l<r && tSum < 0) tSum -= nums[l++];
-            ans = max(tSum, ans);
+            ans = max(ans, tSum = tSum > 0 ? tSum + nums[r] : nums[r]);
+            ++r;
         }
-        if(maxx <= 0) return maxx;
         return ans;
     }
 };
