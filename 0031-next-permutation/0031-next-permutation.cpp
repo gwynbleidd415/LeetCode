2985bd631c3aa94417ea7d443fb9e5cde27fd8e4
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-30 13:57:05 +0530

    Time: 0 ms (100%), Space: 12.7 MB (8.38%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int sml{0}, cmp{0}, n{(int)nums.size()};
-        for(int i{1};i<n;++i){
-            if(nums[i] > nums[i-1]) {
-                sml = i;
-                cmp = i-1;
-            } else if (nums[i] > nums[cmp] && nums[i] < nums[sml]) {
-                sml = i;
-            }
+        int n{(int)nums.size()}, i{n-1};
+        while(i>0 && nums[i]<=nums[i-1]) --i;
+        reverse(nums.begin()+i, nums.end());
+        if(i>0) {
+            int k = upper_bound(nums.begin()+i, nums.end(), nums[i-1]) - nums.begin();
+            swap(nums[i-1], nums[k]);
         }
-        if(sml == 0) reverse(nums.begin(), nums.end());
-        else{
-            swap(nums[sml], nums[cmp]);
-            sort(nums.begin()+cmp+1, nums.end());
-        }
+        // int sml{0}, cmp{0}, n{(int)nums.size()};
+        // for(int i{1};i<n;++i){
+        //     if(nums[i] > nums[i-1]) {
+        //         sml = i;
+        //         cmp = i-1;
+        //     } else if (nums[i] > nums[cmp] && nums[i] < nums[sml]) {
+        //         sml = i;
+        //     }
+        // }
+        // if(sml == 0) reverse(nums.begin(), nums.end());
+        // else{
+        //     swap(nums[sml], nums[cmp]);
+        //     sort(nums.begin()+cmp+1, nums.end());
+        // }
     }
 };
