23e9ca05b92c6cefd198cb98824401b441ce96f4
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-31 15:04:02 +0530

    Time: 13 ms (14.36%), Space: 14.3 MB (91.2%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -4,10 +4,13 @@ public:
         int l{0}, r=nums.size()-1, mid;
         while(l<=r){
             mid = (l+r)>>1;
-            cout<<l<<' '<<r<<' '<<mid<<'\n';
+            // cout<<l<<' '<<r<<' '<<mid<<'\n';
             if(nums[mid] == target) return true;
-            if(nums[mid] == nums[l] && nums[mid] == nums[r]) {++l, --r; continue;}
-            if(nums[l]<nums[mid]){
+            if(nums[l] == nums[mid] && nums[r] == nums[mid]){
+                ++l,--r;
+                continue;
+            }
+            if(nums[l]<=nums[mid]){
                 if(target >= nums[l] && target < nums[mid]) r = mid - 1;
                 else l = mid + 1;
             } else {
@@ -16,5 +19,22 @@ public:
             }
         }
         return false;
+        // int l{0}, r=nums.size()-1, mid;
+        // while(l<=r){
+        //     mid = (l+r)>>1;
+        //     cout<<l<<' '<<r<<' '<<mid<<'\n';
+        //     if(nums[mid] == target) return true;
+        //     if(nums[l]<nums[mid] || nums[r] < nums[mid]){
+        //         if(target >= nums[l] && target < nums[mid]) r = mid - 1;
+        //         else l = mid + 1;
+        //     } else if(nums[l]>nums[mid] || nums[r]>nums[mid]) {
+        //         if(target <= nums[r] && target > nums[mid]) l = mid + 1;
+        //         else r = mid - 1;
+        //     } else {
+        //         ++l, --r;
+        //     }
+        // }
+        // return false;
+
     }
 };
