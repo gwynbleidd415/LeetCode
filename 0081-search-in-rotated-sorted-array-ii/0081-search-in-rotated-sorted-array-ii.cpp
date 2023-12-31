5af5cea1f77a567ace813db123d2c36eaac1e09d
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2023-12-31 15:01:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -6,14 +6,13 @@ public:
             mid = (l+r)>>1;
             cout<<l<<' '<<r<<' '<<mid<<'\n';
             if(nums[mid] == target) return true;
-            if(nums[l]<nums[mid] || nums[r] < nums[mid]){
+            if(nums[mid] == nums[l] && nums[mid] == nums[r]) {++l, --r; continue;}
+            if(nums[l]<nums[mid]){
                 if(target >= nums[l] && target < nums[mid]) r = mid - 1;
                 else l = mid + 1;
-            } else if(nums[l]>nums[mid] || nums[r]>nums[mid]) {
+            } else {
                 if(target <= nums[r] && target > nums[mid]) l = mid + 1;
                 else r = mid - 1;
-            } else {
-                ++l, --r;
             }
         }
         return false;
