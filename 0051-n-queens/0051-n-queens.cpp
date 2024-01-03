37c0034af56229eee31b7fa858ce3760c734c6a0
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-03 20:28:39 +0530

    Time: 8 ms (39.55%), Space: 8.2 MB (43.89%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    vector<string> constructStringVector(vector<int> &qs){
+        int n = qs.size();
+        string str;
+        vector<string> ans;
+        for(int i{0};i<n;++i) {
+            ans.push_back(string(qs[i], '.') + "Q" + string(n-qs[i]-1, '.'));
+        }
+        return move(ans);
+    }
+    void solveNQueensHelper(int n, int i, vector<int> &qs, vector<vector<string>> &ans) {
+        if(i == n) {
+            ans.push_back(constructStringVector(qs));
+            return;
+        }
+        bool isValid;
+        int sum, diff;
+        for(int j{0};j<n;++j) {
+            isValid = true;
+            sum = i+j, diff = i-j;
+            for(int k{0};k<i;++k) {
+                if(qs[k] == j || qs[k] + k == sum || k - qs[k] == diff) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if(isValid) {
+                qs.push_back(j);
+                solveNQueensHelper(n, i+1, qs, ans);
+                qs.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<int> qs;
+        solveNQueensHelper(n, 0, qs, ans);
+        return ans;
+    }
+};
