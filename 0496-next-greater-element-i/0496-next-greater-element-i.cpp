cb17ab7c1983bdcdd05ada834b8a82e2f30765f2
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-04 23:59:01 +0530

    Time: 3 ms (89.58%), Space: 9.2 MB (56.35%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    vector<int> nextGreaterElementSolver(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> nextGreaterElementSolver1(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, int> umap;
         int n = nums2.size();
         vector<int> dp(n);
@@ -16,8 +16,25 @@ private:
         for(int i{0};i<nums1.size();++i) ans[i] = dp[umap[nums1[i]]];
         return ans;
     }
+    vector<int> nextGreaterElementSolver2(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> umap;
+        int n = nums2.size();
+        vector<int> dp(n);
+        int temp{n};
+        for(int i{n-1};i>=0;--i) {
+            umap[nums2[i]] = i;
+            while(temp != n && nums2[temp] < nums2[i]) temp = dp[temp];
+            dp[i] = temp;
+            temp = i;
+        }
+        nums2.push_back(-1);
+        vector<int> ans(nums1.size());
+        for(int i{0};i<nums1.size();++i) ans[i] = nums2[dp[umap[nums1[i]]]];
+        return ans;
+    }
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        return nextGreaterElementSolver(nums1, nums2);
+        // return nextGreaterElementSolver1(nums1, nums2);
+        return nextGreaterElementSolver2(nums1, nums2);
     }
 };
