b1be0587f3004b69162d8f385a7ca55602f01771
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-05 01:21:34 +0530

    Time: 6 ms (92.58%), Space: 17.8 MB (89.04%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -10,8 +10,20 @@ private:
         }
         return ans;
     }
+    vector<int> solution2(vector<int>& asteroids) {
+        vector<int> ans;
+        int n=asteroids.size();
+        int i{0};
+        while(i<n) {
+            if(ans.empty() || asteroids[i]>0 || ans.back()<0) ans.push_back(asteroids[i]), ++i;
+            else if(ans.back() == -asteroids[i]) ans.pop_back(), ++i;
+            else if(ans.back() > -asteroids[i]) ++i;
+            else while(!ans.empty() && ans.back()>0 && ans.back()<-asteroids[i]) ans.pop_back();
+        }
+        return ans;
+    }
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        return solution1(asteroids);
+        return solution2(asteroids);
     }
 };
