0aa12a648f946c2b14a13003f7a7b0b46e6c23b2
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-06 23:13:29 +0530

    Time: 205 ms (8.4%), Space: 55.6 MB (81.64%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -19,12 +19,36 @@ class Solution {
             }
             if(li == nk) return nums[r];
             if(li>nk) r = li-1;
-            else l = li+1;
+            else l = li + 1,  swap(nums[li], nums[r]);
+        }
+        return 0;
+    }
+    int solution3(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n == 1) return nums.front();
+        int nk = n - k;
+        int l{0}, r{n-1}, li, ri, rt;
+        while(l<=r){
+            li = l, ri = r-1, rt = r;
+            while(li <= ri) {
+                if(nums[li] == nums[r]) {
+                    swap(nums[--rt], nums[li]);
+                    if(ri == rt) --ri;
+                    else swap(nums[ri--], nums[li]);
+                }
+                else if(nums[li] > nums[r]) swap(nums[li], nums[ri--]);
+                else ++li;
+            }
+            if(li<=nk && li+r-rt>=nk) return nums[r];
+            if(li>nk) r = li-1;
+            else nk-=r-rt+1, l = li, r = rt-1;
         }
         return 0;
     }
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        return solution1(nums, k);
+        // return solution1(nums, k);
+        // return solution2(nums, k);
+        return solution3(nums, k);
     }
 };
