26c4d52dabbd9d4a0e0b2024f7af49911474129f
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-07 00:02:23 +0530

    Time: 40 ms (96.5%), Space: 34.9 MB (63.35%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    int solution1(vector<char>& tasks, int n) {
+        if(n == 0) return tasks.size();
+        vector<int> count(26);
+        for(char &task: tasks) ++count[task-'A'];
+        priority_queue<int> pq;
+        for(int ct: count) if(ct) pq.push(ct);
+        int ans{0};
+        vector<int> store;
+        while(pq.size()>n) {
+            store.clear();
+            for(int i{0};i<=n;++i) {
+                store.push_back(pq.top()-1), pq.pop();
+            }
+            ++ans;
+            for(int i{0};i<=n;++i) {
+                if(store[i]) pq.push(store[i]);
+                else break;
+            }
+        }
+        ans *= (n+1);
+        if(!pq.empty()) {
+            int temp{pq.top()}, cnt{1};
+            pq.pop();
+            while(!pq.empty() && temp == pq.top()) pq.pop(), ++cnt;
+            ans += (temp-1)*(n+1) + cnt;
+        }
+        return ans;
+    }
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        return solution1(tasks, n);
+    }
+};
