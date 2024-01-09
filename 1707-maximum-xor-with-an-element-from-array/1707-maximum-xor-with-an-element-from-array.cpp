61fc5a69f0c81663d9f96d2efd3e19ef730c0672
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-09 21:37:59 +0530

    Time: 600 ms (72.25%), Space: 211.9 MB (90.63%) - LeetHub

--- 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.cpp
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.cpp
@@ -6,19 +6,19 @@ private:
             arr[0] = arr[1] = nullptr;
         }
     };
-    int maximizeXorMaxx(TrieNode *trie, int xr, int curi) {
+    int maximizeXorMaxx1(TrieNode *trie, int xr, int curi) {
         if(curi == 0) return 0;
         bool ii = (xr&curi) == 0;
-        if(trie->arr[ii]) return curi^maximizeXorMaxx(trie->arr[ii], xr, curi>>1);
-        return maximizeXorMaxx(trie->arr[!ii], xr, curi>>1);
+        if(trie->arr[ii]) return curi^maximizeXorMaxx1(trie->arr[ii], xr, curi>>1);
+        return maximizeXorMaxx1(trie->arr[!ii], xr, curi>>1);
     }
-    int maximizeXor(TrieNode *trie, int xr, int mx, int curi) {
+    int maximizeXor1(TrieNode *trie, int xr, int mx, int curi) {
         if(trie == nullptr) return -1;
         if(curi == 0) return 0;
         bool ind = (mx&curi) != 0;
         if(ind) {
-            int ans1 = maximizeXor(trie->arr[1], xr, mx, curi>>1);
-            int ans2 = trie->arr[0] ? maximizeXorMaxx(trie->arr[0], xr, curi>>1) : -1;
+            int ans1 = maximizeXor1(trie->arr[1], xr, mx, curi>>1);
+            int ans2 = trie->arr[0] ? maximizeXorMaxx1(trie->arr[0], xr, curi>>1) : -1;
             if(ans1 == -1) {
                 if(ans2 == -1) return -1;
                 return (xr&curi)^ans2;
@@ -26,12 +26,12 @@ private:
             if(ans2 == -1) return (xr&curi)^curi^ans1;
             return max((xr&curi)^curi^ans1, (xr&curi)^ans2);
         } else {
-            int ans = maximizeXor(trie->arr[0], xr, mx, curi>>1);
+            int ans = maximizeXor1(trie->arr[0], xr, mx, curi>>1);
             if(ans!=-1) return (xr&curi)^ans;
             else return -1;
         }
     }
-    vector<int> maximizeXorTrie(vector<int>& nums, vector<vector<int>>& queries) {
+    vector<int> maximizeXorTrie1(vector<int>& nums, vector<vector<int>>& queries) {
         TrieNode *head = new TrieNode(), *temp;
         int MAXI = 1<<30, curi;
         int index;
@@ -46,12 +46,59 @@ private:
         }
         vector<int> ans(queries.size());
         for(int i{0};i<queries.size();++i) {
-            ans[i] = maximizeXor(head, queries[i][0], queries[i][1], MAXI);
+            ans[i] = maximizeXor1(head, queries[i][0], queries[i][1], MAXI);
+        }
+        return ans;
+    }
+    void insertIntoTrie(TrieNode *trie, int n) {
+        TrieNode *temp = trie;
+        int curr = 1<<30;
+        bool index;
+        for(;curr!=0 && trie->arr[index=(curr&n)!=0];curr >>= 1) {
+            trie = trie->arr[index];
+        }
+        for(;curr!=0;curr >>= 1) {
+            index = (curr&n)!=0;
+            trie->arr[index] = new TrieNode();
+            trie = trie->arr[index];
+        }
+    }
+    int maximizeXor2(TrieNode *trie, int n) {
+        int curr = 1<<30;
+        int index;
+        int ans{0};
+        while(curr) {
+            index = (n&curr) == 0;
+            if(trie->arr[index]) {
+                ans = ans ^ curr;
+                trie = trie->arr[index];
+            } else {
+                trie = trie->arr[!index];
+            }
+            curr >>= 1;
+        }
+        return ans;
+    }
+    vector<int> maximizeXorTrie2(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = queries.size();
+        vector<int> ind(n), ans(n);
+        iota(ind.begin(), ind.end(), 0);
+        sort(nums.begin(), nums.end());
+        sort(ind.begin(), ind.end(), [&queries](int a, int b) {
+            return queries[a][1] < queries[b][1];
+        });
+        TrieNode *trie = new TrieNode();
+        int j{0};
+        for(int i{0};i<n;++i) {
+            while(j<nums.size() && nums[j] <= queries[ind[i]][1]) insertIntoTrie(trie, nums[j]), ++j;
+            if(j == 0) ans[ind[i]] = -1;
+            else ans[ind[i]] = maximizeXor2(trie, queries[ind[i]][0]);
         }
         return ans;
     }
 public:
     vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
-        return maximizeXorTrie(nums, queries);
+        // return maximizeXorTrie1(nums, queries);
+        return maximizeXorTrie2(nums, queries);
     }
 };
