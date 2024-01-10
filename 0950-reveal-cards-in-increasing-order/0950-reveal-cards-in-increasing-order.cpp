9c7069fd20fbb1d64e6101631e5ebfe2041b3fb4
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-10 10:53:16 +0530

    Time: 7 ms (20.83%), Space: 8.8 MB (85.52%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -15,8 +15,22 @@ private:
         for(i=0;i<deck.size();++i) ans.push_back(dq.front()), dq.pop_front();
         return move(ans);
     }
+    vector<int> solution2(vector<int> &deck) {
+        sort(deck.begin(), deck.end());
+        queue<int> qu;
+        for(int i{0};i<deck.size();++i) qu.push(i);
+        vector<int> ans(deck.size());
+        for(int i{0};i<deck.size();++i) {
+            ans[qu.front()] = deck[i];
+            qu.pop();
+            qu.push(qu.front());
+            qu.pop();
+        }
+        return move(ans);
+    }
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
-        return solution1(deck);
+        // return solution1(deck);
+        return solution2(deck);
     }
 };
