ebebb6488cd8c1b3ba3ccd44f76a881768d90ecc
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-10 16:20:06 +0530

    Time: 4 ms (25.51%), Space: 8.1 MB (69.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,8 +9,18 @@ class Solution {
         }
         return max(taig.first, taig.second);
     }
+    int solution2(vector<int> &nums) {
+        if(nums.size() == 1) return nums.front();
+        vector<int> dp(nums.size() + 1);
+        dp[0] = 0, dp[1] = nums.front();
+        for(int i{1};i<nums.size();++i) {
+            dp[i+1] = max(dp[i], dp[i-1] + nums[i]);
+        }
+        return dp.back();
+    }
 public:
     int rob(vector<int>& nums) {
-        return solution1(nums);
+        // return solution1(nums);
+        return solution2(nums);
     }
 };
