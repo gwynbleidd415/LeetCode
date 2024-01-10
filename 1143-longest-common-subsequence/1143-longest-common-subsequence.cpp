da2239eba7fb5ecda59994df3997e49be78323bb
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-10 22:37:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -11,8 +11,23 @@ private:
         }
         return dp.back().back();
     }
+    int solution2(string &text1, string &text2) {
+        int n = text1.size(), m = text2.size();
+        vector<int> dp(m+1);
+        int temp1{0}, temp2{0};
+        for(int i{0};i<n;++i) {
+            for(int j{0};j<m;++j) {
+                temp1 = dp[j+1];
+                if(text1[i] == text2[j]) dp[j+1] = temp2 + 1;
+                else dp[j+1] = max(dp[j], dp[j+1]);
+                temp2 = temp1;
+            }
+        }
+        return dp.back();
+    }
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        return solution1(text1, text2);
+        // return solution1(text1, text2);
+        return solution2(text1, text2);
     }
 };
