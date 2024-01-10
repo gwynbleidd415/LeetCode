71719cdfe243dac66f427dc20a39aa97412f4e39
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-10 18:25:40 +0530

    Time: 7 ms (73.79%), Space: 10.5 MB (30.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -15,8 +15,23 @@ class Solution {
         }
         return dp.back().back();
     }
+    int solution2(vector<vector<int>>& grid) {
+        vector<int> dp(grid.front().size());
+        dp[0] = grid[0][0];
+        for(int i{1};i<grid.front().size();++i) {
+            dp[i] = dp[i-1] + grid[0][i];
+        }
+        for(int i{1};i<grid.size();++i) {
+            dp[0] = grid[i][0];
+            for(int j{1};j<grid.front().size();++j){
+                dp[j] = grid[i][j] + min(dp[j], dp[j-1]);
+            }
+        }
+        return dp.back();
+    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         return solution1(grid);
+        return solution2(grid);
     }
 };
