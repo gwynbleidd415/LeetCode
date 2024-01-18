552dcc5277f86634226c2d546dd37d5e90f85774
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-18 13:13:53 +0530

    Time: 10 ms (89.33%), Space: 18.4 MB (5.6%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    void solution1DFS(TreeNode *root, TreeNode *p, TreeNode *q, vector<TreeNode *> &arr1, vector<TreeNode *> &arr2, bool &popAr1, bool &popAr2) {
+        if(root == nullptr) return;
+        if(popAr1) arr1.push_back(root);
+        if(popAr2) arr2.push_back(root);
+        if(root == p) {
+            arr2.pop_back();
+            popAr1 = false;
+            return;
+        }
+        if(root == q) {
+            arr1.pop_back();
+            popAr2 = false;
+            return;
+        }
+        solution1DFS(root->left, p, q, arr1, arr2, popAr1, popAr2);
+        solution1DFS(root->right, p, q, arr1, arr2, popAr1, popAr2);
+        if(popAr1) arr1.pop_back();
+        if(popAr2) arr2.pop_back();
+    }
+    TreeNode* solution1(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode *> arr1, arr2;
+        bool popAr1{true}, popAr2{true};
+        solution1DFS(root, p, q, arr1, arr2, popAr1, popAr2);
+        if(arr1.size() == 0) return q;
+        if(arr2.size() == 0) return p;
+        for(int i{1};i<arr1.size();++i) {
+            // cout << arr1[i]->val << ' ' << arr2[i]->val << '\n';
+            if(arr1[i]!=arr2[i]) return arr1[i-1];
+        }
+        return arr1.back();
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return solution1(root, p, q);
+    }
+};
