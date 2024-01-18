a8f160b3c5859d74b051e414627fa71068bad2db
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-18 20:18:32 +0530

    Time: 7 ms (95.14%), Space: 26.5 MB (52.11%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,6 +10,7 @@
  * };
  */
 class Solution {
+private:
     TreeNode* solution1Helper(vector<int>& preorder, vector<int>& inorder, int il, int ir, int &pi) {
         if(il > ir) return nullptr;
         int i{il};
@@ -21,13 +22,27 @@ class Solution {
         node->right = solution1Helper(preorder, inorder, i+1, ir, pi);
         return node;
     }
-private:
     TreeNode* solution1(vector<int>& preorder, vector<int>& inorder) {
         int pi{0};
         return solution1Helper(preorder, inorder, 0, inorder.size() - 1, pi);
     }
+    TreeNode* solution2Helper(vector<int>& preorder, vector<int>& inorder, unordered_map<int, int> &umap, int il, int ir, int &pi) {
+        if(il > ir) return nullptr;
+        int i{umap[preorder[pi++]]};
+        TreeNode *node = new TreeNode(inorder[i]);
+        node->left = solution2Helper(preorder, inorder, umap, il, i-1, pi);
+        node->right = solution2Helper(preorder, inorder, umap, i+1, ir, pi);
+        return node;
+    }
+    TreeNode* solution2(vector<int>& preorder, vector<int>& inorder) {
+        int pi{0};
+        unordered_map<int, int> umap;
+        for(int i{0};i<inorder.size();++i) umap[inorder[i]] = i;
+        return solution2Helper(preorder, inorder, umap, 0, inorder.size() - 1, pi);
+    }
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return solution1(preorder, inorder);
+        // return solution1(preorder, inorder);
+        return solution2(preorder, inorder);
     }
 };
