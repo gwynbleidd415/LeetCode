6166748e5717afea37553bcc0b16202a19d19526
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-18 21:13:43 +0530

    Time: 0 ms (100%), Space: 8.7 MB (78.82%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,13 +10,18 @@
  * };
  */
 class Solution {
-    // void inorderTraversalHelper(TreeNode *root, vector<int> &ans){
-    //     if(root == nullptr) return;
-    //     inorderTraversalHelper(root->left, ans);
-    //     ans.push_back(root->val);
-    //     inorderTraversalHelper(root->right, ans);
-    // }
-    void inorderTraversalHelperStack(TreeNode *root, vector<int> &ans){
+    void solution1Helper(TreeNode *root, vector<int> &ans){
+        if(root == nullptr) return;
+        solution1Helper(root->left, ans);
+        ans.push_back(root->val);
+        solution1Helper(root->right, ans);
+    }
+    vector<int> solution1(TreeNode* root) {
+        vector<int> ans;
+        solution1Helper(root, ans);
+        return ans;
+    }
+    void solution2Helper(TreeNode *root, vector<int> &ans){
         if(root == nullptr) return;
         stack<TreeNode *> st;
         TreeNode *temp, *curr{root};
@@ -31,11 +36,39 @@ class Solution {
             curr = temp->right;
         }
     }
+    vector<int> solution2(TreeNode* root) {
+        vector<int> ans;
+        solution2Helper(root, ans);
+        return ans;
+    }
+    vector<int> solution3(TreeNode* root) {
+        vector<int> ans;
+        TreeNode *curr{root}, *prev;
+        while(curr != nullptr) {
+            if(curr->left == nullptr) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                prev = curr->left;
+                while(prev->right != nullptr && prev->right != curr) {
+                    prev = prev->right;
+                }
+                if(prev->right == nullptr) {
+                    prev->right = curr;
+                    curr = curr->left;
+                } else {
+                    prev->right = nullptr;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        // inorderTraversalHelper(root, ans);
-        inorderTraversalHelperStack(root, ans);
-        return ans;
+        // return solution1(root);
+        // return solution2(root);
+        return solution3(root);
     }
 };
