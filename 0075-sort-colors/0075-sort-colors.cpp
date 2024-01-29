cfd8565dc8f54d0534aa8f55d6ccf2f598fa5c32
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-01-29 21:50:06 +0530

    Time: 0 ms (100%), Space: 9.9 MB (5.66%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,28 +1,29 @@
 class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        // vector<int> hm(3, 0);
-        // for(int num:nums) ++hm[num];
-        // int k{-1};
-        // for(int i{0};i<3;++i){
-        //     for(int j{0};j<hm[i];++j){
-        //         nums[++k] = i;
-        //     }
-        // }
-
-        // int itr0{0}, itr1{0}, itr2{(int)nums.size()-1};
-        // while(itr1 <= itr2){
-        //     if(nums[itr1] == 0){
-        //         swap(nums[itr0], nums[itr1]);
-        //         ++itr0, ++itr1;
-        //     } else if(nums[itr1] == 1){
-        //         ++itr1;
-        //     } else {
-        //         swap(nums[itr1],nums[itr2]);
-        //         --itr2;
-        //     }
-        // }
-
+    void solution1(vector<int>& nums) {
+        vector<int> hm(3, 0);
+        for(int num:nums) ++hm[num];
+        int k{-1};
+        for(int i{0};i<3;++i){
+            for(int j{0};j<hm[i];++j){
+                nums[++k] = i;
+            }
+        }
+    }
+    void solution2(vector<int>& nums) {
+        int itr0{0}, itr1{0}, itr2{(int)nums.size()-1};
+        while(itr1 <= itr2){
+            if(nums[itr1] == 0){
+                swap(nums[itr0], nums[itr1]);
+                ++itr0, ++itr1;
+            } else if(nums[itr1] == 1){
+                ++itr1;
+            } else {
+                swap(nums[itr1],nums[itr2]);
+                --itr2;
+            }
+        }
+    }
+    void solution3(vector<int>& nums) {
         vector<int> hm(3, 0);
         for(int num:nums) ++hm[num];
         hm[1] += hm[0];
@@ -33,5 +34,11 @@ public:
             else if(i < hm[1]) nums[i] = 1;
             else nums[i] = 2;
         }
+    }
+public:
+    void sortColors(vector<int>& nums) {
+        // solution1(nums);
+        solution2(nums);
+        // solution3(nums);
     }
 };
