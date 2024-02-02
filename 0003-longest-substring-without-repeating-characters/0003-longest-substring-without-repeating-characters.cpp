b9774d843a20fc4fd0e43208b457c0b052338a4b
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-02-02 19:09:24 +0530

    Time: 4 ms (92.25%), Space: 10.3 MB (48.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,5 @@
 class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
+    int solution1(string &s) {
         int n = s.size();
         if(n <= 1) return n;
 
@@ -10,7 +9,7 @@ public:
         visited[s[0]] = true;
 
         while(swe < n){
-            while(visited[s[swe]] == true){
+            while(visited[s[swe]]){
                 visited[s[sws++]] = false;
             }
             visited[s[swe++]] = true;
@@ -18,5 +17,30 @@ public:
         }
 
         return ans;
+    }
+    int solution2(string &s) {
+        int n = s.size();
+        if(n <= 1) return n;
+
+        int ans = 1;
+        int sws = 0, swe = 1;
+        vector<int> visited(256, -1);
+        visited[s[0]] = 0;
+
+        while(swe < n){
+            if(visited[s[swe]] != -1){
+                sws = max(sws, visited[s[swe]] + 1);
+            }
+            visited[s[swe]] = swe;
+            ans = max(ans, swe-sws + 1);
+            ++swe;
+        }
+
+        return ans;
+    }
+public:
+    int lengthOfLongestSubstring(string s) {
+        // return solution1(s);
+        return solution2(s);
     }
 };
