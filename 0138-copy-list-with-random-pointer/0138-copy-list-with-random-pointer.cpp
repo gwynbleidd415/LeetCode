8db41fb678804fd45facd38ef84621ed89e4956a
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-02-08 18:17:23 +0530

    Time: 10 ms (28.36%), Space: 12.8 MB (31.09%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -32,8 +32,33 @@ class Solution {
         }
         return umap[head];
     }
+    Node* solution2(Node* head) {
+        if(head == nullptr) return nullptr;
+        Node *iter{head}, *newNode;
+        while(iter) {
+            newNode = new Node(iter->val);
+            newNode->next = iter->next;
+            iter->next = newNode;
+            iter = iter->next->next;
+        }
+        Node *newHead{head->next}, *nextIter;
+        iter = head;
+        while(iter) {
+            if(iter->random) iter->next->random = iter->random->next;
+            iter = iter->next->next;
+        }
+        iter = head;
+        while(iter) {
+            nextIter = iter->next->next;
+            if(nextIter) iter->next->next = nextIter->next;
+            iter->next = nextIter;
+            iter = nextIter;
+        }
+        return newHead;
+    }
 public:
     Node* copyRandomList(Node* head) {
-        return solution1(head);
+        // return solution1(head);
+        return solution2(head);
     }
 };
