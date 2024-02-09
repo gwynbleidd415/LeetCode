e1f4158fbe20df157a8636ccb8f78d207acd4673
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-02-10 03:52:50 +0530

    Time: 8 ms (32.8%), Space: 12.6 MB (32.94%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -21,8 +21,29 @@ private:
         solution1Helper(candidates, target, ans, curr, 0);
         return ans;
     }
+    void solution2Helper(vector<int> &candidates, int target, vector<vector<int>> &ans, vector<int> &curr, int ind) {
+        if(target == 0) {
+            ans.push_back(curr);
+            return;
+        }
+        for(int i{ind};i<candidates.size();++i) {
+            if(target < candidates[ind]) break;
+            if(i != ind && candidates[i] == candidates[i-1]) continue;
+            curr.push_back(candidates[i]);
+            solution2Helper(candidates, target-candidates[i], ans, curr, i+1);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> solution2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> curr;
+        vector<vector<int>> ans;
+        solution2Helper(candidates, target, ans, curr, 0);
+        return ans;
+    }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        return solution1(candidates, target);
+        // return solution1(candidates, target);
+        return solution2(candidates, target);
     }
 };
