5490cc94c4fa08390ff3095e00bdc10c68084d31
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-02-10 01:11:43 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.25%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -21,8 +21,25 @@ private:
         solution1Helper(nums, 0, ans, curr);
         return ans;
     }
+    void solution2Helper(vector<int>& nums, int i, vector<vector<int>> &ans, vector<int> &curr) {
+        ans.push_back(curr);
+        for(int j{i};j<nums.size();++j) {
+            if(j != i && nums[j] == nums[j-1]) continue;
+            curr.push_back(nums[j]);
+            solution2Helper(nums, j+1, ans, curr);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> solution2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> curr;
+        solution2Helper(nums, 0, ans, curr);
+        return ans;
+    }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        return solution1(nums);
+        // return solution1(nums);
+        return solution2(nums);
     }
 };
