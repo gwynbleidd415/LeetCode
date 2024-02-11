eb7f1556681f428c2a010627542bd77ff3e0befd
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-02-11 16:20:01 +0530

    Time: 5 ms (61.03%), Space: 16.1 MB (32.35%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -3,16 +3,27 @@ private:
     int solution1(vector<int>& nums) {
         int maxNeg{1}, maxPos{1}, ans{nums.front()};
         for(int num:nums){
-            if(num < 0) swap(maxPos, maxNeg);
             maxPos *= num, maxNeg *= num;
+            if(num < 0) swap(maxPos, maxNeg);
             ans = max(ans, maxPos);
             if(maxPos <= 0) maxPos = 1;
             if(maxNeg >= 0) maxNeg = 1;
         }
         return ans;
     }
+    int solution2(vector<int>& nums) {
+        int prefix{1}, suffix{1}, ans{nums[0]};
+        for(int i{0}, j=nums.size()-1;i<nums.size();++i, --j) {
+            prefix *= nums[i], suffix *= nums[j];
+            ans = max({ans, prefix, suffix});
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+        }
+        return ans;
+    }
 public:
     int maxProduct(vector<int>& nums) {
-        return solution1(nums);
+        // return solution1(nums);
+        return solution2(nums);
     }
 };
