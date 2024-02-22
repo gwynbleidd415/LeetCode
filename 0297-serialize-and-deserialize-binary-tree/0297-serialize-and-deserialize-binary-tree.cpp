dadd7959d80460810a010d920899ceb3118c6333
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-02-22 13:51:05 +0530

    Time: 19 ms (95.71%), Space: 30.9 MB (49.44%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -8,10 +8,8 @@
  * };
  */
 class Codec {
-public:
-
-    // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
+private:
+    string serializeSolution1(TreeNode *root) {
         queue<TreeNode *> qu;
         qu.push(root);
         string str{""};
@@ -28,9 +26,7 @@ public:
         }
         return str;
     }
-
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
+    TreeNode* deserializeSolution1(string data) {
         if(data[0] == '#') return nullptr;
         istringstream iss(data);
         string value;
@@ -53,6 +49,53 @@ public:
             }
         }
         return root;
+    }
+    void serializeSolution2Helper(TreeNode *root, string &ans) {
+        if(root == nullptr) {
+            ans.append("#,");
+            return;
+        }
+        ans.append(to_string(root->val)).push_back(',');
+        serializeSolution2Helper(root->left, ans);
+        serializeSolution2Helper(root->right, ans);
+    }
+    string serializeSolution2(TreeNode *root) {
+        string ans{""};
+        serializeSolution2Helper(root, ans);
+        return ans;
+    }
+    void deserializeSolution2Helper(TreeNode *parent, istringstream &iss, bool isLeft) {
+        string value;
+        getline(iss, value, ',');
+        if(value == "#") return;
+        if(isLeft) {
+            parent->left = new TreeNode(stoi(value));
+            deserializeSolution2Helper(parent->left, iss, true);
+            deserializeSolution2Helper(parent->left, iss, false);
+        } else {
+            parent->right = new TreeNode(stoi(value));
+            deserializeSolution2Helper(parent->right, iss, true);
+            deserializeSolution2Helper(parent->right, iss, false);
+        }
+    }
+    TreeNode* deserializeSolution2(string data) {
+        TreeNode parent;
+        istringstream iss(data);
+        deserializeSolution2Helper(&parent, iss, true);
+        return parent.left;
+    }
+
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        // return serializeSolution1(root);
+        return serializeSolution2(root);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        // return deserializeSolution1(data);
+        return deserializeSolution2(data);
     }
 };
 
