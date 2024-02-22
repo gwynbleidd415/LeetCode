faeee3653d792982036836c64c32d19446664339
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-02-22 12:12:55 +0530

    Time: 455 ms (26.45%), Space: 108.2 MB (88.15%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -24,9 +24,34 @@ private:
         int maxx = right.second.first == INT_MAX ? root->val : right.second.second;
         return {currSum, {minn, maxx}};
     }
+    pair<int, pair<int, int>> solution1Helper2(TreeNode* root, int &ans) {
+        if(root->left == nullptr && root->right == nullptr) {
+            ans = max(ans, root->val);
+            return {root->val, {root->val, root->val}};
+        }
+        pair<int, pair<int, int>> left;
+        pair<int, pair<int, int>> right;
+        if(root->left) {
+            left = solution1Helper2(root->left, ans);
+        } else {
+            left = {0, {root->val, INT_MIN}};
+        }
+        if(root->right) {
+            right = solution1Helper2(root->right, ans);
+        } else {
+            right = {0, {INT_MAX, root->val}};
+        }
+        if(left.second.second >= root->val || right.second.first <= root->val) {
+            return {0, {INT_MIN, INT_MAX}};
+        };
+        int currSum = left.first + right.first + root->val;
+        ans = max(ans, currSum);
+        return {currSum, {left.second.first, right.second.second}};
+    }
     int solution1(TreeNode* root) {
         int answer{0};
-        solution1Helper(root, answer);
+        // solution1Helper(root, answer);
+        solution1Helper2(root, answer);
         return answer;
     }
 public:
