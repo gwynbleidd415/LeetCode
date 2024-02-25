1cea74e7c46f178101386b8a8f2d8aa62116db8e
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-02-25 22:16:06 +0530

    Time: 3 ms (85.27%), Space: 23 MB (24.33%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -10,8 +10,24 @@ private:
         }
         return ans;
     }
+    string solution2(string &s) {
+        int i{0}, n = s.size();
+        while(i<n && s[i] == ' ') ++i;
+        if(i == n) return "";
+        string ans{""};
+        while(true) {
+            while(i<n && s[i] == ' ') ++i;
+            if(i>=n) break;
+            int j{i+1};
+            while(j<n && s[j] != ' ') ++j;
+            ans = s.substr(i, j-i) + (ans == "" ? "" : " ") + ans;
+            i = j+1;
+        }
+        return ans;
+    }
 public:
     string reverseWords(string s) {
-        return solution1(s);
+        // return solution1(s);
+        return solution2(s);
     }
 };
