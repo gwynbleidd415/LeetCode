81a311a6da0880042061dde8526c6f573520ad11
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-03-02 16:57:31 +0530

    Time: 21 ms (96.65%), Space: 22 MB (78.11%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,45 +1,49 @@
 class Solution {
 private:
     vector<int> solution1(string &s, vector<string>& words) {
-        int n = words.size();
-        int l = words.front().size();
-        int m = l * n;
-        int k = s.size();
+        int wordsCount = words.size();
+        int wordLength = words.front().size();
+        int permutationStringSize = wordLength * wordsCount;
+        int mainStringSize = s.size();
         vector<int> ans;
-        if(k<m) return ans;
-        unordered_map<string, pair<vector<int>, int>> umap;
-        unordered_map<string, pair<vector<int>, int>>::iterator uitr;
-        vector<int> isPresent(n, -1);
-        vector<int> whatPresent(k, -1);
-        for(int i{0};i<n;++i) umap[words[i]].first.push_back(i);
-        int last = k-m, last2 = k-l;
-        for(int i{0};i<l;++i) {
-            for(int y{i};y<=last2;y+=l) {
-                if((uitr = umap.find(s.substr(y, l))) != umap.end()) {
-                    whatPresent[y] = uitr->second.first[uitr->second.second];
-                    ++uitr->second.second;
-                    if(uitr->second.second == uitr->second.first.size()) uitr->second.second = 0;
+
+        if(mainStringSize < permutationStringSize) return ans;
+
+        unordered_map<string, pair<vector<int>, int>> wordPositions;
+        unordered_map<string, pair<vector<int>, int>>::iterator wordPositionsIterator;
+        vector<int> wordLastPosition(wordsCount, -1);
+        vector<int> whichWordStart(mainStringSize, -1);
+
+        for(int i{0};i<wordsCount;++i) wordPositions[words[i]].first.push_back(i);
+        int iterationEnd = mainStringSize-wordLength;
+
+        for(int i{0};i<wordLength;++i) {
+            for(int y{i};y<=iterationEnd;y+=wordLength) {
+                if((wordPositionsIterator = wordPositions.find(s.substr(y, wordLength))) != wordPositions.end()) {
+                    whichWordStart[y] = wordPositionsIterator->second.first[wordPositionsIterator->second.second];
+                    ++wordPositionsIterator->second.second;
+                    if(wordPositionsIterator->second.second == wordPositionsIterator->second.first.size()) wordPositionsIterator->second.second = 0;
                 }
             }
-            for(int &ip: isPresent) ip = -1;
+            for(int &ip: wordLastPosition) ip = -1;
             int count{0}, j{i};
-            for(;j<i+m;j+=l) {
-                if(whatPresent[j] != -1) {
-                    if(isPresent[whatPresent[j]] == -1) ++count;
-                    isPresent[whatPresent[j]] = j;
+            for(;j<i+permutationStringSize;j+=wordLength) {
+                if(whichWordStart[j] != -1) {
+                    if(wordLastPosition[whichWordStart[j]] == -1) ++count;
+                    wordLastPosition[whichWordStart[j]] = j;
                 }
             }
-            if(count == n) ans.push_back(j-m);
-            for(;j<=last2;j+=l) {
-                int x = j-m;
-                if(whatPresent[x] != -1 && isPresent[whatPresent[x]] == x) {
+            if(count == wordsCount) ans.push_back(j-permutationStringSize);
+            for(;j<=iterationEnd;j+=wordLength) {
+                int x = j-permutationStringSize;
+                if(whichWordStart[x] != -1 && wordLastPosition[whichWordStart[x]] == x) {
                     --count;
                 }
-                if(whatPresent[j] != -1) {
-                    if(isPresent[whatPresent[j]] <= x) ++count;
-                    isPresent[whatPresent[j]] = j;
+                if(whichWordStart[j] != -1) {
+                    if(wordLastPosition[whichWordStart[j]] <= x) ++count;
+                    wordLastPosition[whichWordStart[j]] = j;
                 }
-                if(count == n) ans.push_back(j-m+l);
+                if(count == wordsCount) ans.push_back(j-permutationStringSize+wordLength);
             }
         }
         sort(ans.begin(), ans.end());
