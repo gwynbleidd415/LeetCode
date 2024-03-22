3ca74a0639906863a935d2c4aadb6747a69d2fde
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-03-22 22:39:06 +0530

    Time: 0 ms (100%), Space: 8.1 MB (81.39%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -15,8 +15,34 @@ private:
         vector<vector<bool>> visited(s1.size()+1, vector<bool>(s2.size()+1));
         return solution1Helper(s1, s2, s3, 0, 0, dp, visited);
     }
+    bool solution2(string &s1, string &s2, string &s3) {
+        if(s3.size()!= s1.size() + s2.size()) return false;
+        vector<vector<bool>> dp(s1.size()+1, vector<bool>(s2.size()+1));
+        dp.front().front() = true;
+        for(int i{0};i<s1.size();++i) {
+            dp[i+1][0] = (s1[i] == s3[i] && dp[i][0]);
+        }
+        for(int i{0};i<s2.size();++i) {
+            dp[0][i+1] = (s2[i] == s3[i] && dp[0][i]);
+        }
+        for(int i{0};i<s1.size();++i) {
+            for(int j{0};j<s2.size();++j) {
+                // cout<<i<<' '<<j<<' '<<s1[i]<<' '<<s2[j]<<' '<<s3[i+j]<<'\n';
+                if(s3[i+j+1] == s1[i] && dp[i][j+1]) dp[i+1][j+1] = true;
+                else if(s3[i+j+1] == s2[j] && dp[i+1][j]) dp[i+1][j+1] = true;
+            }
+        }
+        // for(auto i: dp) {
+        //     for(auto j: i) {
+        //         cout << j << ' ';
+        //     }
+        //     cout << '\n';
+        // }
+        return dp.back().back();
+    }
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        return solution1(s1, s2, s3);
+        // return solution1(s1, s2, s3);
+        return solution2(s1, s2, s3);
     }
 };
