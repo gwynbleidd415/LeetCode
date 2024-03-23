e4cd54dae603f131ae8d26d29dd08d1a8dcf509f
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-03-23 12:21:08 +0530

    Time: 390 ms (5.03%), Space: 176.4 MB (5.03%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -49,8 +49,39 @@ private:
         vector<vector<vector<vector<bool>>>> vis(n, vector<vector<vector<bool>>>(n, vector<vector<bool>>(n, vector<bool>(n))));
         return solution1Helper(s1, s2, 0, n-1, 0, n-1, dp, vis);
     }
+    bool solution2Helper(string &s1, string &s2, int str1, int end1, int str2, int end2, vector<vector<vector<vector<bool>>>> &dp, vector<vector<vector<vector<bool>>>> &vis) {
+        if(str1 == end1) return s1[str1] == s2[str2];
+        if(vis[str1][end1][str2][end2]) return dp[str1][end1][str2][end2];
+        vis[str1][end1][str2][end2] = true;
+        vector<int> count(26);
+        int diff{0}, i1{0}, i2{0}, i4{};
+        for(int i{str1}, j{str2};i<=end1;++i,++j) {
+            i1 = s1[i]-'a', i2 = s2[j]-'a';
+            if(i1 == i2) continue;
+            diff += (count[i1]==0) + (count[i2]==0) - (++count[i1]==0) - (--count[i2]==0);
+        }
+        if(diff) return false;
+        int res = false;
+        for(int i{str1}, j{str2};i<end1;++i,++j) {
+            i4 = end2-(j-str2);
+            dp[str1][end1][str2][end2] = solution2Helper(s1, s2, str1, i, i4, end2, dp, vis) && solution2Helper(s1, s2, i+1, end1, str2, i4-1, dp, vis);
+            if(dp[str1][end1][str2][end2]) return true;
+            dp[str1][end1][str2][end2] = solution2Helper(s1, s2, str1, i, str2, j, dp, vis) && solution2Helper(s1, s2, i+1, end1, j+1, end2, dp, vis);
+            if(dp[str1][end1][str2][end2]) return true;
+            
+        }
+
+        return false;
+    }
+    bool solution2(string &s1, string &s2) {
+        int n = s1.size();
+        vector<vector<vector<vector<bool>>>> dp(n, vector<vector<vector<bool>>>(n, vector<vector<bool>>(n, vector<bool>(n))));
+        vector<vector<vector<vector<bool>>>> vis(n, vector<vector<vector<bool>>>(n, vector<vector<bool>>(n, vector<bool>(n))));
+        return solution2Helper(s1, s2, 0, n-1, 0, n-1, dp, vis);
+    }
 public:
     bool isScramble(string s1, string s2) {
-        return solution1(s1, s2);
+        // return solution1(s1, s2);
+        return solution2(s1, s2);
     }
 };
