a25f02d1b07fa692e164eed5c4051e551297ee32
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-04-07 22:00:30 +0530

    Time: 3 ms (87.22%), Space: 15.8 MB (25.25%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    int solution1(vector<int>& prices) {
+        int n = prices.size();
+        stack<int> st;
+        vector<int> gr(n);
+        gr.back() = n-1;
+        st.push(n-1);
+        for(int i{n-2};i>=0;--i) {
+            while(!st.empty() && prices[st.top()] <= prices[i]) st.pop();
+            if(st.empty()) gr[i] = i;
+            else gr[i] = st.top();
+            st.push(i);
+        }
+        int i{0}, profit{0};
+        while(i<n) {
+            if(gr[i] == i) {
+                ++i;
+                continue;
+            }
+            for(int j{i+1};j<gr[i];++j) {
+                if(prices[j] < prices[i]) {
+                    i = j;
+                    continue;
+                }
+            }
+            profit += prices[gr[i]] - prices[i];
+            i = gr[i];
+        }
+        return profit;
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        return solution1(prices);
+    }
+};
