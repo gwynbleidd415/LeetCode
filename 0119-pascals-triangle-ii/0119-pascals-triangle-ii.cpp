369fe65b36f5289b28f5711e16d411c349222482
Gwynbleidd <83465452+gwynbleidd415@users.noreply.github.com>
2024-04-07 21:19:33 +0530

    Time: 0 ms (100%), Space: 7.5 MB (88.92%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -12,8 +12,20 @@ private:
         }
         return curr;
     }
+    vector<int> solution2(int rowIndex) {
+        vector<int> ans(rowIndex+1);
+        ans.front() = 1;
+        for(int i{0};i<rowIndex;++i) {
+            ans[i+1] = 1;
+            for(int j{i};j>0;--j) {
+                ans[j] += ans[j-1];
+            }
+        }
+        return ans;
+    }
 public:
     vector<int> getRow(int rowIndex) {
-        return solution1(rowIndex);
+        // return solution1(rowIndex);
+        return solution2(rowIndex);
     }
 };
